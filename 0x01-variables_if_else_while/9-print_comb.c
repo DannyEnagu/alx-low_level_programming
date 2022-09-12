645aa98e632bfd38bc8ae194201bbec9ba8c24b6
Daniel <danielenagu@gmail.com>
2022-09-11 20:58:50 -0700

    Printing numbers 0-9 in c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print single-digit numbers 0-9
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num + 48);
+
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		++num;
+	}
+	putchar('\n');
+
+	return (0);
+}
