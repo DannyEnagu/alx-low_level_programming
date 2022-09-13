fa3002dbfb6f30e0d9d1c821e306e6240a394ccf
Daniel <danielenagu@gmail.com>
2022-09-13 10:22:26 -0700

    printing with _putchar prototype

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: pint the string _putchar using 
+ * 		the putchar fuction prototype.
+ *
+ * Return: Always 0 (success)		 
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; ++i)
+	{
+		_putchar(str[i]);
+	}	
+	
+	_putchar('\n');
+
+	return (0);
+}	
