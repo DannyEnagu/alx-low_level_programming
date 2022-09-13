b1def8aa292f7b48b97c17f1a488fe7918817c77
Daniel <danielenagu@gmail.com>
2022-09-13 13:33:00 -0700

    comput abs value integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs- A funtion to find the absolute
+ *	value of an integer
+ *
+ * @n: accepted value - type int
+ * 
+ * Return: Always 0 (Success)
+*/
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n)
+}
