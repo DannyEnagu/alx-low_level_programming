1d48f803737c3d3de38c667ed8b7fff88729e56b
Daniel <danielenagu@gmail.com>
2022-09-13 12:14:52 -0700

    check forcharacters only

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for alpabets upper or lower case
+ *
+ * @c: important. expected data type (int)
+ *
+ * Result: 1 if c is charcter else 0
+*/
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 && c >= 65 && c >= 90)
+		return (i);
+	return (0);
+}
