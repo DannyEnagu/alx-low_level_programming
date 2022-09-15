f47a9e01e85406e272f90dc53ae46faeb3bc83a2
Daniel <danielenagu@gmail.com>
2022-09-15 08:48:12 -0700

    pprint \ daigonally n times

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,25 +1,26 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * print_diagonal - print '\' diagonal n times
+ * print_diagonal - print a diagonal line
  *
- * @n: funtion param, place holder type int
+ * @n: is the number of times the \ character
+ *     should be printed
 */
 
 void print_diagonal(int n)
 {
-	int i, j;
+	int postn, space;
 
-	if (n > 0)
+	if (n <= 0)
+		_putchar('\n');
+	else
 	{
-		for (i = 1; i <= n; i++)
+		for (postn = 1; postn <= n; ++postn)
 		{
-			for (j = 1; j <= i; j++)
-				_putchar('_')
-			_putchar(92);
+			for (space = 1; space <= postn; ++space)
+				_putchar(' ');
+			_putchar(92); /*is equal to '/' char*/
 			_putchar('\n');
 		}
 	}
-	else
-		_putchar('\n');
 }
