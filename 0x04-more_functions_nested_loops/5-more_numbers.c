bac5d923768002ca9431b226f39862614da77dcb
Daniel <danielenagu@gmail.com>
2022-09-15 07:35:21 -0700

    clean up code

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,19 +7,13 @@
 
 void more_numbers(void)
 {
-	int val, i, j;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			n = j;
-			if (j > 9)
-			{
-				_putchar(1 + 48);
-				n = j % 10;
-			}
-			_putchar(n + 48);
+			_putchar(j + 48);
 		}
 		_putchar('\n');
 	}
