5bee9a24627625482e8822add71bb97654762424
Daniel <danielenagu@gmail.com>
2022-09-15 06:26:04 -0700

    print 0 to 14, 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,13 +7,19 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int val, i, j;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + 48);
+			n = j;
+			if (j > 9)
+			{
+				_putchar(1 + 48);
+				n = j % 10;
+			}
+			_putchar(n + 48);
 		}
 		_putchar('\n');
 	}
