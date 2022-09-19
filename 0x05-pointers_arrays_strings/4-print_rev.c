383dd838bd6762c2c22bb440888c06d006221ad3
Daniel <danielenagu@gmail.com>
2022-09-19 10:09:01 -0700

    Fix betty errors

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,8 +13,8 @@ void print_rev(char *s)
 	int i;
 
 	/*finds the length of string without null character*/
-	for (i = 0; s[i] != '\0'; ++i);
-
+	for (i = 0; s[i] != '\0'; ++i)
+		;
 	/*print char from the last index as you decrement*/
 	for (--i; i >= 0; --i)
 		_putchar(s[i]);
