1226c961b308ed71c28f94a8beaf9f0a5df01d3b
Daniel <danielenagu@gmail.com>
2022-10-04 09:17:00 -0700

    How to concatenate all main argv

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * argstostr - concatenate all argument vectors
+ * @ac: argument count
+ * @av: argument vectors
+ *
+ * Return: pointer to new string or
+ *      NULL on failure
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int h, i, j, len;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (h = 0; h < ac; h++)
+	{
+		i = 0;
+		while (av[h][i] != '\0')
+		{
+			len++;
+			i++;
+		}
+		len++;
+	}
+
+	str = malloc(sizeof(**av) * len + 1);
+
+	if (str != NULL)
+	{
+		j = 0;
+		for (h = 0; h < ac; h++)
+		{
+			i = 0;
+			while (av[h][i] != '\0')
+			{
+				str[j] = av[h][i];
+				i++;
+				j++;
+			}
+			str[j] = '\n';
+			j++;
+		}
+		return (str);
+	}
+	else
+		return (NULL);
+}
