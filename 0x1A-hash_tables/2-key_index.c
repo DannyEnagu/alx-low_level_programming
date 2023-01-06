4dfbf6b87b71b56c3dc84f82bf41e65c0c2d1ae1
dannyEnagu <danielEnagu@gmail.com>
2023-01-06 19:52:21 +0000

    Added function to get Hash Table array index to store key/value pair

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+* key_index - Computes the index of a given key string
+*
+* @key: the key string input
+* @size: Hash Table array size
+*
+* Return: The index at which the key/value pair should be
+*	stored in the array of the hash table
+*/
+
+unsigned long int key_index(const unsigned char *key,
+	unsigned long int size)
+{
+	unsigned long int index;
+
+	index = hash_djb2(key);
+	return (index % size);
+}
