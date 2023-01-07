c23815e355b9876df3340a32330913d26beee413
dannyEnagu <danielEnagu@gmail.com>
2023-01-07 04:21:44 +0000

    fix betty errors

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,12 +1,14 @@
 #include"hash_tables.h"
 
 /**
-* hash_table_get - A function that retrieves a value associated with a key.
+* hash_table_get - A function that retrieves a
+*	value associated with a key.
 *
 * @ht: Hash Table to look into
 * @key: The key to search for
 *
-* Return: value associated with the element, or NULL if key couldn’t be found.
+* Return: value associated with the element, or
+*	NULL if key couldn’t be found.
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
